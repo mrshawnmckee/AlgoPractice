ee649e5c9930935b9b2d43243703986030b14cfb
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2023-04-09 21:59:17 -0400

    Add files via upload

---
+++ fundamentalsToDo.js
@@ -0,0 +1,87 @@
+// // Setting and Swapping //
+
+// let myNum = 42;
+// let myName = 'Shawn';
+
+// temp = myNum;
+// myNum = myName;
+// myName = temp;
+
+// console.log(myNum, myName);
+
+// // Print -52 to 1066 //
+
+// for( var i = -52; i < 1067; i++){
+//     console.log(i);
+// }
+
+
+// // Dont Worry, Be Happy //
+// function beCheerful(){
+//     console.log('Good Morning!');
+// }
+
+// for (let i = 0; i<98; i++){
+//     beCheerful();
+// }
+
+// Multiple of Three- but not all //
+
+// for(let i = -300; i < 1; i ++){
+//     if(i == -3){
+//         null;
+//     } 
+//     else if (i == -6) {
+//         null;
+//     } 
+//     else if(i %3 == 0) {
+//         console.log(i)
+//     }
+//     else{
+//         null
+//     }
+// }
+
+// Printing Integers with While //
+
+// let i = 2000;
+// while(i < 5280){
+//     console.log(i);
+//     i++
+// }
+
+
+
+// You say its your Birthday //
+
+// function birthdate(a, b){
+//     if(a==8 && b==15 || a==15 && b==8){
+//         console.log("How did you know?")
+//     } else {
+//         console.log("Just another day")
+//     }
+// }
+// birthdate(15, 15)
+
+
+
+// Leap Year //
+
+// function year(i){
+//     if(i % 4 == 0){
+//         if(i % 100 == 0){
+//             if (i % 400 == 0){
+//                 console.log("leap Year")
+//             }else{
+//             console.log("not a leap year")
+//             }
+//         }                
+//     } else{
+//         console.log("leap year")
+//     }
+// }
+// year(100)
+
+
+// Print and Count //
+
