809a655400d48f08f96e686ac6f497c1bcbb8b5d
Shawn McKee <shawnroosemckee@gmail.com>
2023-09-22 16:48:27 -0400

    added mergesort file

---
+++ basicAlgos/mergeSort.js
@@ -0,0 +1,9 @@
+//Merge sort uses recursive; faster(O(n log n)) but uses more space(O(n))
+// pseudocode:
+// if(n < 2){
+//     return
+// }else{
+//     sort left half of Elements
+//     sort right half of elements
+//     merge sorted halves
+// }
